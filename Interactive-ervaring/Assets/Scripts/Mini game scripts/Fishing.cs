6e69758aa4bdf4714cde3ca225abeb38f0175094
jcdocter <joeydocter@gmail.com>
2023-05-17 16:10:37 +0200

    sensitivity added for the motion

---
+++ Interactive-ervaring/Assets/Scripts/Mini game scripts/Fishing.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Fishing : MonoBehaviour
+{
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+    private Vector3 position;
+
+    private float fallSpeed = 0.6f;
+    private Vector2 acceleration;
+
+    private void Start()
+    {
+        gyroEnabled = EnableGyro();
+        position = transform.position;
+    }
+
+    private void Update()
+    {
+        if (gyroEnabled)
+        {
+            if(transform.position.y <= -1.6f)
+            {
+                position.y = -1.6f;
+                transform.position = position;
+                transform.position += new Vector3(acceleration.x, 0.0f, 0.0f);
+
+                if (Input.acceleration.y >= 0.0f)
+                {
+                    acceleration += new Vector2(0.0f, Input.acceleration.y);
+                    transform.position += new Vector3(0.0f, acceleration.y, 0.0f);
+                }
+            }
+            else
+            {
+                transform.position -= new Vector3(0.0f, fallSpeed * Time.deltaTime, 0.0f);
+            }
+
+            acceleration = new Vector2(gyro.attitude.y, Input.acceleration.y);
+        }
+
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            return true;
+        }
+
+        return false;
+    }
+}
+
