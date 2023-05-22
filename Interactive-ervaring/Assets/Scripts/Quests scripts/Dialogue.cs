91bb49dc070b4411f26f95c0957cfaead6646a4e
Meta <Raoulscholtens@gmail.com>
2023-05-22 16:28:53 +0200

    Added dialogue script

--- Interactive-ervaring/Assets/Scripts/Quests scripts/Dialogue.cs
+++ Interactive-ervaring/Assets/Scripts/Quests scripts/Dialogue.cs
@@ -8,14 +8,12 @@ public class Dialogue : MonoBehaviour
     [TextArea(3,5)]
     public string[] text;
 
-    private TextMeshProUGUI dialogueBox;
+    public TextMeshProUGUI dialogueBox;
 
     private int index = 0;
 
     private void Start()
     {
-        dialogueBox = GetComponentInChildren<TextMeshProUGUI>();
-
         dialogueBox.text = text[index];
     }
 
