eda2fa8b4ce7f4d40ca224c6a5f6c7eed19c3802
jcdocter <joeydocter@gmail.com>
2023-05-26 13:16:05 +0200

    fixed reset timer on the fish hangle and added the sefie function

--- Interactive-ervaring/Assets/Scripts/Fish scripts/FishController.cs
+++ Interactive-ervaring/Assets/Scripts/Fish scripts/FishController.cs
@@ -6,11 +6,12 @@ public class FishController : MonoBehaviour
 {
     public float movementSpeed;
     private GameObject movePoint;
-    private Rules rules = new Rules();
+    private Fishing fishing;
     private Vector3 spawnPoint;
 
     private void Start()
     {
+        fishing = FindObjectOfType<Fishing>();
         spawnPoint = new Vector3(Random.Range(-2.75f, 2.75f), Random.Range(-5.3f, 5.3f), 9.0f);
 
         GameObject moveToPoint = Instantiate(new GameObject(), spawnPoint, Quaternion.identity);
@@ -42,7 +43,7 @@ public class FishController : MonoBehaviour
         if (_other.gameObject.GetComponent<Fishing>())
         {
             Inventory.GetInstance().amountOfFish++;
-            rules.CheckOffQuest();
+            fishing.rules.CheckOffQuest();
         }
     }
 }
